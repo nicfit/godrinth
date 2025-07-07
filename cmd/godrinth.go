516322f48ed3991f4f9bd7670e1390395b85fd45
Travis Shirk <trshirk@cisco.com>
2025-07-06 18:05:33 -0600

    config

--- cmd/godrinth.go
+++ cmd/godrinth.go
@@ -9,14 +9,43 @@ import (
 	"github.com/urfave/cli/v3"
 )
 
+func getConfig(cmd *cli.Command) *godrinth.Config {
+	for _, c := range cmd.Lineage() {
+		if cfg, exists := c.Metadata["config"]; exists {
+			if config, ok := cfg.(*godrinth.Config); ok {
+				return config
+			}
+		}
+	}
+	return nil
+}
+
 var mainCommand = &cli.Command{
 	Name: "godrinth",
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:    "config",
+			Aliases: []string{"c"},
+			Usage:   "Load configuration from `FILE`",
+			Value:   godrinth.DEFAULT_CONFIG_FILE,
+		},
+	},
 	Commands: []*cli.Command{
 		getCommand,
 		searchCommand,
 	},
+	Before: func(ctx context.Context, command *cli.Command) (context.Context, error) {
+		// Load configuration
+		config, err := godrinth.LoadConfig(command.String("config"))
+		if err != nil {
+			return ctx, fmt.Errorf("failed to load configuration: %w", err)
+		}
+		command.Metadata["config"] = config
+
+		return ctx, nil
+	},
 	Action: func(ctx context.Context, command *cli.Command) error {
-		fmt.Println("FIXME: TUI")
+		fmt.Println("FIXME: TUI", command.Metadata["config"])
 		return nil
 	},
 }
@@ -24,6 +53,8 @@ var mainCommand = &cli.Command{
 var getCommand = &cli.Command{
 	Name: "get",
 	Action: func(ctx context.Context, cmd *cli.Command) error {
+		fmt.Println("FIXME: GET", getConfig(cmd))
+
 		project, err := godrinth.GetProject(ctx, cmd.Args().First())
 		if err != nil {
 			return err
@@ -37,6 +68,8 @@ var getCommand = &cli.Command{
 var searchCommand = &cli.Command{
 	Name: "search",
 	Action: func(ctx context.Context, cmd *cli.Command) error {
+		fmt.Println("FIXME: SEARCH", getConfig(cmd))
+
 		projects, err := godrinth.SearchProject(ctx, cmd.Args().First(), &godrinth.SearchOptions{
 			Facets: "[[\"project_type=mod\"]]",
 			Index:  "relevance",
--- go.mod
+++ go.mod
@@ -4,4 +4,8 @@ go 1.22
 
 toolchain go1.24.3
 
-require github.com/urfave/cli/v3 v3.3.8 // indirect
+require (
+	github.com/adrg/xdg v0.5.3 // indirect
+	github.com/urfave/cli/v3 v3.3.8 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+)
